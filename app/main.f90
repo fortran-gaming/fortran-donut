f478952634d7a71e7adfa3975490617f6cd20cf8
Michael Hirsch <scivision@users.noreply.github.com>
2022-05-30 15:02:49 -0400

    formatted print

--- app/main.f90
+++ app/main.f90
@@ -18,16 +18,22 @@ character, allocatable :: screen(:)
 real(wp) :: sini,cosj,sinA,sinj,cosA,cosj2, mess, cosi,cosB,t,sinB
 integer :: x,y,o,N,ii, u, Nloop
 character(5) :: buf
-character(:), allocatable :: dumpfn
 
-Nloop = 100
+character(:), allocatable :: dumpfn
+logical :: dump
+
+Nloop = 300
+dump = .false.
 do ii = 1, command_argument_count()
   call get_command_argument(ii, buf)
 
   select case (buf)
   case ("-dump")
     call get_command_argument(ii+1, buf, status=k)
-    if (k==0) dumpfn = trim(buf)
+    if (k==0) then
+      dumpfn = trim(buf)
+      dump = .true.
+    endif
   case ("-frames")
     call get_command_argument(ii+1, buf, status=k)
     if (k==0) read(buf, '(I5)') Nloop
@@ -41,7 +47,7 @@ L = rows*cols
 
 allocate(z(0:L-1), screen(0:L-1))
 
-if(allocated(dumpfn)) open(newunit=u, file=dumpfn, status='replace', action='write')
+if(dump) open(newunit=u, file=dumpfn, status='replace', action='write')
 
 write(stdout,"(a)", advance="no") ESC // "[2J"  !< move cursor to top left
 
@@ -79,16 +85,14 @@ do ii = 1,Nloop
   write(stdout, "(a)", advance="no") ESC // "[d" !< line feed
 
   do k=0, rows-2
-    print *, screen(k*cols:k*cols+cols)
-    if(allocated(dumpfn)) write(u, "(80a)") screen(k*cols:k*cols+cols)
+    print '(81a)', screen(k*cols:k*cols+cols)
+    if(dump) write(u, "(81a)") screen(k*cols:k*cols+cols)
   enddo
-
-  ! print *, size(screen), k*cols,k*cols+cols
 
   a = a + 0.04
   b = b + 0.02
 enddo
 
-if(allocated(dumpfn)) close(u)
+if(dump) close(u)
 
 end program
